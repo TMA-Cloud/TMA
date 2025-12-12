60a0e124782d23104b7dab3d4a2c9a07beb4e875
Zinadin Zidan <zidan.roking@gmail.com>
2025-12-12 16:39:12 +0600

    Fix database DB name
    - Match with others
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   port: Number(process.env.DB_PORT) || 5432,
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'cloud_storage',
+  database: process.env.DB_NAME || 'cloud_store',
   ssl: process.env.DB_SSLMODE === 'require' ? { rejectUnauthorized: false } : false
 });
 
